ebcc7876e9868bb11c97f6852061f374d28c8031
victor <victor.h@clarusway.com>
2022-02-17 11:14:36 +0300

    feat: project setup

---
+++ home/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.fields import CharField
+
+# Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=40)
+    phone_number = models.CharField(max_length=40)
+    email = models.EmailField(max_length=254)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
